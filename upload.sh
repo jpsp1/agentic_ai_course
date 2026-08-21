76bd545924c430e5d7f3579ef23753c4a67ef79f
joao.pagaime@gmail,com <joao.pagaime@gmail.com>
2026-08-21 22:43:47 +0100

    updates a Fri Aug 21 22:43:47 WEST 2026

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 d=`date`
-#git config user.email "joao.pagaime@gmail.com"
 git config --global user.name "joao.pagaime@gmail.com"
-
+git config user.name "joao.pagaime@gmail,com"
+git config user.email "joao.pagaime@gmail.com"
 git commit -m "updates a $d" -a
 git push -u origin main
